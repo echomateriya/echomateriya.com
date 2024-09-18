619c7377bfba32ea0cf29ee6544ad824d31bf4f5
Victor Shcherb <victor.shcherb@gmail.com>
2024-09-18 13:18:06 +0300

    Fix tests

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Import your components
 import MainSite from './MainSite';
